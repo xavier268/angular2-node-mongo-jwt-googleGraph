33d775610c0bcb9e6e63cfe475baa5abb5d20d4c
xavier <xavier@gandillot.com>
2015-12-01 16:08:06 +0100

    Debut utilisation jasmine pour les tests

--- run.sh
+++ run.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+echo "---------------Versions--------------------"
+echo "Bower version : $(bower --version)"
+echo "jasmine version : $(jasmine -v)"
+echo "npm version : $(npm --version)"
+echo "node version : $(node --version)"
+echo "---------------Install---------------------"
 bower install
 npm install
+
+echo "---------------Test------------------------"
+npm test
+echo "---------------Running---------------------"
+echo ""
 npm start
