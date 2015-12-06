44e62838c481f890a4157a53bc06f3b700557a9c
xavier <xavier@gandillot.com>
2015-12-06 13:18:15 +0100

    Added verification that mongo is installed

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ echo "Bower version : $(bower --version)"
 echo "mocha version : $(mocha --version)"
 echo "npm version : $(npm --version)"
 echo "node version : $(node --version)"
+echo  $(mongo --version)
 echo "---------------Install---------------------"
 bower install
 npm install
